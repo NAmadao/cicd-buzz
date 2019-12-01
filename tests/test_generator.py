856d0cc589129170fc8b3b185676d7973b697c1e
prithwish90 <prithwish.ganguly@gmail.com>
2019-11-30 20:14:40 -0500

    success

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -8,8 +8,13 @@ def test_sample_multiple_words():
     assert len(words) == 2
     assert words[0] in l
     assert words[1] in l
-    assert words[0] is words[1]
+    assert words[0] is not words[1]
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
+    
+def test_sample_single_word():
+    l = ('foo', 'bar', 'foobar')
+    word = generator.sample(l)
+    assert word in l
--- tests/test_generator2.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from buzz import generator
-
-def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
-    assert word in l
